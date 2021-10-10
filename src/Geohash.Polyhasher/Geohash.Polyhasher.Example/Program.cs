ee90f951d1bd5042bcee77216499ae57aa48a228
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2021-10-10 21:59:14 +0200

    Added example project.

---
+++ src/Geohash.Polyhasher/Geohash.Polyhasher.Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using Geohash.Polyhasher;
+using NetTopologySuite.IO;
+
+// Define the polygon we wish to encode
+var polygon = new WKTReader().Read("MULTIPOLYGON (((40 40, 20 45, 45 30, 40 40)), " +
+                                   "((20 35, 45 20, 30 5, 10 10, 10 30, 20 35), " +
+                                   "(30 20, 20 25, 20 15, 30 20)))");
+
+// Create a new polyhasher instance
+var polyhasher = new Polyhasher();
+
+// Returns a HashSet<string> of 27 geohashes which encodes the multipolygon specified above
+var geohashes = polyhasher.Encode(polygon, 2, PolyhasherMode.Intersects);
+
